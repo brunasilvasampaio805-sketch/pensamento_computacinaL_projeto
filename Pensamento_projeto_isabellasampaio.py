3b183ac3f1c1c1295fa5a07d48f30847ac0f6686
Aluno em Vocacao isabella Silva Sampaio <brunasilvasampaio805@gmail.com>
2026-07-08 17:07:22 -0300

    upgrade_projeto

---
+++ Pensamento_projeto_isabellasampaio.py
@@ -0,0 +1,66 @@
+'''
+
+Um Bloco de Comentarios,
+Projeto:Salão de beleza
+
+>PO (Como dono: Quero um sistema de vendas para o meu salão de beleza, para que eu possa 
+controlar as vendas.)
+
+>QA (Como cliente: Quero um sistema de vendas para o meu salão de beleza, para que eu
+ possa mudar o cabelo e aparência de forma rápida e fácil.)
+
+>Tech (Como programador: Quero um sistema de vendas para o meu salão de beleza para que
+ eu possa desenvolver um software eficiente e funcional para o salão.)
+ 
+>Dev (Como programador: Quero um sistema de vendas para meu salão de beleza, para que eu
+possa implementar as funcionalidades necessárias para que eu possa atender ao gosto de
+todos os clientes.)
+
+>UX (Como designer de experiência do usuário: Quero um sistema de vendas para meu salão
+de beleza, para que eu possa criar uma interface intuitiva e agradável para todos que
+quiserem mudar de cabelo.)
+
+>IA (Como analista de dados: Quero um sistema de vendas para meu salão de beleza, para
+que eu possa coletar e analisar os dados de venda de cada corte, ajudando a identificar 
+padrões de consumo e otimizar estratégias de marketing e estoque.)
+
+'''
+
+print('Olá, Mundo!')
+print('Bem-Vindo ao Sistema de vendas - Salão de beleza!\n')
+while True:
+  
+print('1 - Listar cortes de cabelo')
+print('2 - Mostrar preços')
+print('3 - Listar horários disponiveis')
+print('4 - Agendar um dia')
+print('5 - Ver o valor a ser pago')
+print('0 - Sair do sistema')
+opção = input ('Digite a opção desejada)'
+'if p1_nome = input(Corte de cabelo: Borboleta')
+   p1_preco = 100,00
+   p1_descricao = (Corte com camadas longas e curto no topo. Ideal para cabelos medíos e longos)
+
+elif p2_nome= (channel)
+p2_preço=90,00
+p2_descricao= (corte deixando cabelo acima do ombro)
+
+ elif p3_nome= (channel vegana)
+p3_preço=100,00
+p3_descricao = (corte deixando acima do ombro, com uma franja acima da sombrancelha.)
+elif p4_nome = (moicano tradicinal)
+p4_preço = 58,00
+p4_descricao = (laterais raspadas ou bem curtas,faixa central mais longa e levantada)
+
+elif  p5_nome= americano
+p5_preço=40,(function) raspadas: Any
+p5_descricao= (topo do cabelo mais volumoso enquanto o degradê é aplicado de forma isolada.)
+
+print ( h
+
+
+
+
+
+
+'
